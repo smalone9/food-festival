b3382824517e2f51e9137fbde2964411854a15aa
Samantha Malone <sammac99@gmail.com>
2021-09-01 13:02:31 -0700

    added webpack objects

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,10 @@
 const path = require("path");
+
+module.exports = {
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.resolve(__dirname, "dis"),
+    filename: "main.bundle.js",
+  },
+  mode: development,
+};
