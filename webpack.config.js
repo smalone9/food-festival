d6fda40a87ae83a56471ad266aee60a2e747efe5
Samantha Malone <sammac99@gmail.com>
2021-09-04 21:13:39 -0700

    added webpack bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack = require("webpack");
 
@@ -11,6 +13,9 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
     }),
   ],
   mode: "development",
